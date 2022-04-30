336cbc5b308df919eae67c31925be27616efb5ce
KendrickPC <kennethpchang@gmail.com>
2022-04-29 21:04:05 -0700

    Fry image added as placeholder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Nav from "./components/Nav";
 import Form from "./components/Form";
+import Hero from "./components/Hero"
 
 export default function App() {
   return (
     <div>
       <Nav />
       <Form />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="heroRow">
+      <div className="heroColumn">
+        <img className="fry" src="./images/fry.jpeg" alt="fry" />
+      </div>
+    </div>
+  )
+}
