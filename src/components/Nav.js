eff2b24f442739dfeddbbaaa97990152de36ea1e
KendrickPC <kennethpchang@gmail.com>
2022-04-29 18:16:11 -0700

    troll logo added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from "react";
+// import Troll from "../images/troll.png";
 
 export default function Nav() {
   return (
     <div>
       <div className="header">
+        <img className="troll" src="/images/troll.png" />
         <h1 className="projectTitle">Meme Generator</h1>
         <h3 className="subTitle">React Course - Project 3</h3>
       </div>
